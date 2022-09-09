cc2554ef1384b90350a28e2d92a2f62ad58af847
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-09-09 08:27:01 +0200

    setup file structure, new dependency for styling

---
+++ client/components/Hero.tsx
@@ -0,0 +1,15 @@
+import { FC } from "react"
+import styled from "styled-components";
+
+
+const Hero: FC = () => {
+    return (<Container></Container>)
+}
+
+const Container = styled.div`
+	/* Layout */
+	display: flex;
+	flex-direction: column;
+`
+
+export default Hero;
---
+++ client/components/Navbar.tsx
@@ -0,0 +1,20 @@
+import { FC } from "react"
+import styled from "styled-components";
+import { ConnectButton } from '@rainbow-me/rainbowkit';
+
+const Navbar: FC = () => {
+    return (
+		<Container>
+			<ConnectButton />
+		</Container>)
+}
+
+
+const Container = styled.div`
+	/* Layout */
+	display: flex;
+	flex-direction: row;
+`
+
+
+export default Navbar;
