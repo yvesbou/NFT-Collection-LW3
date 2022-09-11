40420f6bc041ae8daa97159ac440ab78638656fd
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-09-11 21:19:42 +0200

    changes to hero

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -5,7 +5,7 @@ import styled from "styled-components";
 const Hero: FC = () => {
     return (
         <Container>
-            <h1>Crypto Devs NFT Collection</h1>
+            <Title>Crypto Devs NFT Collection</Title>
         </Container>)
 }
 
@@ -14,6 +14,16 @@ const Container = styled.div`
 	display: flex;
 	flex-direction: column;
     justify-content: center;
+    align-content: center;
+    height: 90%;
+    background-color: red;
+`
+
+const Title = styled.div`
+    font-size: 2rem;
+    display: flex;
+    justify-content: center;
+    background-color: blue;
 `
 
 export default Hero;
