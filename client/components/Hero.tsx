6254bc7a9cec2eda5f94e43bf5d3deb64c47f3db
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-09-12 07:21:50 +0200

    nice hero

--- client/components/Hero.tsx
+++ client/components/Hero.tsx
@@ -5,7 +5,10 @@ import styled from "styled-components";
 const Hero: FC = () => {
     return (
         <Container>
-            <Title>Crypto Devs NFT Collection</Title>
+            <Title>
+                crypto devs
+            </Title>
+            <Highlight>NFT collection</Highlight>
         </Container>)
 }
 
@@ -16,14 +19,29 @@ const Container = styled.div`
     justify-content: center;
     align-content: center;
     height: 90%;
-    background-color: red;
+    // background-color: red;
 `
 
 const Title = styled.div`
-    font-size: 2rem;
     display: flex;
     justify-content: center;
-    background-color: blue;
+    // background-color: blue;
+    padding: 20px;
+    margin-top: 110px;
+    font-family: 'Lato';
+    font-weight: 900;
+    font-size: 6rem;
+`
+
+const Highlight = styled.div`
+    display: flex;
+    justify-content: center;
+    font-family: 'Lato';
+    font-weight: 900;
+    font-size: 6rem;
+    background: -webkit-linear-gradient(30deg, rgba(2,0,36,1) 0%, rgba(9,9,121,1) 35%, rgba(0,212,255,1) 100%);
+    -webkit-background-clip: text;
+    -webkit-text-fill-color: transparent;
 `
 
 export default Hero;
