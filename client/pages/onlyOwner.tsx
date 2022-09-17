d3218f5244e620188424672f8cb99a3b44574b03
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-09-17 10:27:09 +0200

    wip only owner page

--- client/pages/onlyOwner.tsx
+++ client/pages/onlyOwner.tsx
@@ -12,16 +12,28 @@ const OnlyOwner: NextPage = () => {
     return(
         <main className={styles.main}>
             <Navbar/>
-            <Title>This is the "Only Owner Page" and is only visible to {address?.slice(0,5)}...{address?.slice(-5,-1)} the address which deployed the smart contracts</Title>
+            <Info>This is the "Only Owner Page" and is only visible to {address?.slice(0,5)}...{address?.slice(-5,-1)} the address which deployed the smart contracts</Info>
         </main>
     );
 }
 
-const Title = styled.div`
+const Info = styled.div`
     display: flex;
     flex-direction: row;
     justify-content: center;
     margin: 20px;
+    padding: 10px;
+    border-radius: 12px;
+    border: transparent;
+    background-color: #F8F8F8;
+    box-shadow: rgba(100, 100, 111, 0.3) 0px 7px 29px 0px;
+    background-color: orange;
+`
+
+const OnlyOwnerButton = styled.button`
+	padding: 10px;
+	letter-spacing: 0.1px;
+	font-weight: 900;
 `
 
 export default OnlyOwner;
