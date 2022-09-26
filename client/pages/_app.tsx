cb968ae06e6bcb97eac6c474e55cd77bb20e2d0a
yvesbou <63062257+yvesbou@users.noreply.github.com>
2022-09-26 16:23:54 +0200

    enable frontend connection to local blockchain

--- client/pages/_app.tsx
+++ client/pages/_app.tsx
@@ -5,18 +5,26 @@ import { RainbowKitProvider, getDefaultWallets } from '@rainbow-me/rainbowkit';
 import { chain, configureChains, createClient, WagmiConfig } from 'wagmi';
 import { alchemyProvider } from 'wagmi/providers/alchemy';
 import { publicProvider } from 'wagmi/providers/public';
+import { jsonRpcProvider } from 'wagmi/providers/jsonRpc'
 
 const { chains, provider, webSocketProvider } = configureChains(
   [
     chain.goerli,
   ],
   [
+    jsonRpcProvider({
+      priority: 0,
+      rpc: (chain) => ({
+        http: `http://localhost:8545`,
+      }),
+    }),
     alchemyProvider({
       // This is Alchemy's default API key.
       // You can get your own at https://dashboard.alchemyapi.io
       apiKey: '_gg7wSSi0KMBsdKnGVfHDueq6xMB9EkC',
     }),
     publicProvider(),
+    
   ]
 );
 
